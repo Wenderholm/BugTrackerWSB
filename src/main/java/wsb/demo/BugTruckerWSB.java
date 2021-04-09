a33df1f98828440b01c106bf32a81587ab97113c
przemyslaw wendeholm <pwenderholm@gmai.com>
2021-04-09 19:24:39 +0200

    initial commit

---
+++ src/main/java/wsb/demo/BugTruckerWSB.java
@@ -0,0 +1,13 @@
+package wsb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTruckerWSB {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugTruckerWSB.class, args);
+    }
+
+}
