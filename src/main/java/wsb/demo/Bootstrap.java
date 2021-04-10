f76b7506c91bc8d31b1bc6d29975e2adf93d21d5
przemyslaw wendeholm <pwenderholm@gmai.com>
2021-04-10 08:55:05 +0200

    changed application properties, add bootstrap service

---
+++ src/main/java/wsb/demo/Bootstrap.java
@@ -0,0 +1,16 @@
+package wsb.demo;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Bootstrap implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("aplikacja jest uruchomiona");
+
+    }
+
+}
